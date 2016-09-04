7777fb20b72c960e4c6ab5eac29ebf4059e275bc
Nik Martelaro <nmartelaro@gmail.com>
2016-09-04 15:23:24 -0700

    Initial commit

---
+++ isNowAGoodTime.py
@@ -0,0 +1,42 @@
+import speech_recognition as sr
+import timeit
+import pyttsx
+import os
+import thread
+
+def processSpeech():
+    thread.start_new_thread(os.system,('afplay Morse.aiff',)) # play a sound to let the driver know they can speak
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+    # recognize speech using Google Speech Recognition
+    start_time = timeit.default_timer()
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        response = r.recognize_google(audio)
+        print("Google Speech Recognition thinks you said " + response)
+        os.system('afplay Pop.aiff') # play a beep when you process the response to let the driver know they were heard.
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    print("Google: " + str(timeit.default_timer() - start_time))
+
+# def onEnd(name, completed):
+#     processSpeech()         # listen and process the answer
+#     engine.endLoop()        # stop the tts engine
+#     print("finishing", name, completed)
+
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+os.system('say is now a good time?')
+processSpeech()
+
+# # initialize the speech system
+# engine = pyttsx.init()
+# engine.connect('finished-utterance', onEnd)
+# engine.say('Is now a good time?', 'now')
+# engine.startLoop()
