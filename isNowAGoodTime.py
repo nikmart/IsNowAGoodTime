c76d93af7c0dfb32e09d4b48eac585a1a73451ee
Nik Martelaro <nmartelaro@gmail.com>
2016-09-24 00:36:00 -0700

    Took out speech rec. Just recording the audio and will transcribe later.

--- isNowAGoodTime.py
+++ isNowAGoodTime.py
@@ -4,56 +4,66 @@ import os
 import random
 import sched, time
 import _thread
+import pyaudio
+import wave
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+
+def recordAudio():
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    output_file = str(time.time()) + "_answer.wav"
+    wf = wave.open(output_file, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
 def queryDriver():
     # write the ask time to the file
-    f.write(str(time.time()) + ',')
+    f.write(str(time.time()) + '\n')
 
-    # obtain audio from the microphone
-    r = sr.Recognizer()
     os.system('say is now a good time?')
 
     _thread.start_new_thread(os.system,('afplay Morse.aiff',)) # play a sound to let the driver know they can speak
 
-    try:
-        with sr.Microphone() as source:
-            print("Say something!")
-            audio = r.listen(source, timeout=5.0)
-
-        # recognize speech using Google Speech Recognition [2]
-        start_time = timeit.default_timer()
-        try:
-            # for testing purposes, we're just using the default API key
-            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-            # instead of `r.recognize_google(audio)`
-            response = r.recognize_google(audio)
-            print("Google Speech Recognition thinks you said " + response)
-            f.write(response + '\n')
-            os.system('afplay Pop.aiff') # play a beep when you process the response to let the driver know they were heard.
-            # write audio to a WAV file
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-            f.write('N/A,error\n')
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
-            f.write('N/A,error\n')
-        print("Google: " + str(timeit.default_timer() - start_time))
-
-    except sr.WaitTimeoutError:
-        print("Speech timeout, assume 'no'")
-        f.write('no,timeout\n')
+    recordAudio()
 
 
 # set up a file to log the question times and the answers [3]
-f = open('isNowAGoodTime_labels' + str(int(time.time())) + '.csv', 'w')
-f.write('time,label,note\n')
+f = open('isNowAGoodTime_queryTimes' + str(int(time.time())) + '.csv', 'w')
+f.write('time')
 
 # set up a scheduler to pick random times to say is now a good time [1]
 s = sched.scheduler(time.time, time.sleep)
 
 while True:
     print(time.time())
-    ask_time = random.randint(30,3*60) # wait time in seconds
+    ask_time = random.randint(1,3) # wait time in seconds
     print(ask_time)
     s.enter(ask_time, 1, queryDriver, ())
     s.run()
