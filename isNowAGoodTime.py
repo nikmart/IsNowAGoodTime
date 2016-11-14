c016dd1e6e2db718af08a15d265c0ca621bc32fe
Nik Martelaro <nmartelaro@gmail.com>
2016-11-13 22:24:36 -0800

    Cleaning repo

--- isNowAGoodTime.py
+++ isNowAGoodTime.py
@@ -48,6 +48,26 @@ def recordAudio():
 
     _thread.start_new_thread(os.system,('afplay Pop.aiff',)) # play a sound to let the driver know they can speak
 
+    AUDIO_FILE = os.path.join(path.dirname(path.realpath(__file__)), output_file)
+    recognizeAudio(AUDIO_FILE)
+
+def recognizeAudio():
+    # use the audio file as the audio source
+    r = sr.Recognizer()
+    with sr.AudioFile(AUDIO_FILE) as source:
+        audio = r.record(source) # read the entire audio file
+
+    # recognize speech using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
 def queryDriver():
     # write the ask time to the file
     f.write(str(time.time()) + '\n')
