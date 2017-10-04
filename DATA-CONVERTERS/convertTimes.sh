65b961530f79d4bbea41b1f4b811a25fa361bb8f
Nik Martelaro <nmartelaro@gmail.com>
2017-10-03 19:51:29 -0700

    Adding data frame creator notebook

--- DATA-CONVERTERS/convertTimes.sh
+++ DATA-CONVERTERS/convertTimes.sh
@@ -18,10 +18,10 @@ echo "Video start = $videostart"
 
 echo "Converting data...\n"
 #(python3 convertBio.py $directory/BIOPHYS/*.csv $videostart ; echo "Biosignals Converted!") &
-(python3 convertIMU.py $directory/IMU/*.csv $videostart ; echo "IMU Converted!") &
-(python3 convertResponses.py $directory/RESPONSES/*.csv $videostart ; echo "Responses Converted!") &
-(python3 convertGPS.py $directory/GPS/*.csv $videostart ; echo "GPS Converted!") &
-(python processCAN.py $directory/CAN/*.txt $videostart ; echo "CAN Converted!") &&
+(python convertIMU.py $directory/IMU/*.csv $videostart ; echo "IMU Converted!") &
+(python convertResponses.py $directory/RESPONSES/*.csv $videostart ; echo "Responses Converted!") &
+(python convertGPS.py $directory/GPS/*.csv $videostart ; echo "GPS Converted!") &
+(python2 processCAN.py $directory/CAN/*.txt $videostart ; echo "CAN Converted!") &&
 
 echo "Moving data...\n"
 
