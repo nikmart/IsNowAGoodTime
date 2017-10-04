52ea6cb4682725338e95660de46653b77ee8f184
Nik Martelaro <nmartelaro@gmail.com>
2017-10-04 13:08:35 -0700

    Updated convert responses

--- DATA-CONVERTERS/convertResponses.py
+++ DATA-CONVERTERS/convertResponses.py
@@ -1,5 +1,18 @@
+"""
+convertResponses.py
+
+Author: Nik Martelaro (nikmart@stanford.edu)
+
+Purpose: Convert the UNIX timestamps for each time a question was asked
+
+Requirements: Python 3
+
+Usage: python convertResponses.py [response_data] [starttime]
+[response_data]: csv file with the UNIX timestamps for each question
+[starttime]: UNIX timestamp from the start of the quad video
+"""
+
 import sys
-from datetime import datetime, timedelta
 
 response_data = sys.argv[1]
 starttime = sys.argv[2]
@@ -14,5 +27,12 @@ with open(response_data, newline='\n') as csvfile:
             if float(data_time) - float(starttime) > 0:
                 time = float(data_time) - float(starttime)
                 output.write("{}\n".format(time))
+
+# Check that the data seems like the right length
+print("Data time = {} minutes".format(round(time/60,1)))
+if time/60 < 40:
+    print("Response data looks short...possibly not all there")
+else:
+    print("Response data length looks OK")
 
 output.close()
